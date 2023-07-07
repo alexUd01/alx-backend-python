2142d582452e3754d394d0742c36cf3209a7717d
alexUd01 <alexanderikpeama@gmail.com>
2023-07-07 02:19:01 +0100

    correction

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """ Annotation Exercice """
-from typing import Any, Mapping, Union
+from typing import Any, Mapping, TypeVar, Union
+
+
+T = TypeVar('T', bound=Any)
 
 
 def safely_get_value(
         dct: Mapping,
         key: Any,
-        default: Union[Any, None] = None
-) -> Union[Any, None]:
+        default: Union[T, None] = None
+) -> Union[Any, T]:
     """" The function to annotate """
     if key in dct:
         return dct[key]
