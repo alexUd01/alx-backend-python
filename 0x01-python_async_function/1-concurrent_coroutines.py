06412c31dd6dcb277c7f71ca4afc68853f250244
alexUd01 <alexanderikpeama@gmail.com>
2023-07-10 21:44:34 +0100

    Task 4

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -21,4 +21,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ The async routine """
     result = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
-    return result
+    return sorted(result)
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Take the code from wait_n and alter it into a new function `task_wait_n`.
+The code is nearly identical to wait_n except task_wait_random is being
+called.
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ The async routine """
+    result = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    return sorted(result)
