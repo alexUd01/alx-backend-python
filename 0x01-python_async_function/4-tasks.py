2fc07c5e0c2909ad679ebc5ca6167d4abf884aa6
alexUd01 <alexanderikpeama@gmail.com>
2023-07-10 23:45:28 +0100

    Corrected task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,7 +4,7 @@ The code is nearly identical to wait_n except task_wait_random is being
 called.
 """
 import asyncio
-from typing import List
+from typing import List, Union
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
@@ -12,7 +12,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ The async routine """
-    result = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
+    result = await asyncio.gather(*(task_wait_random(max_delay)
+                                    for i in range(n)))
     return sorted(result)
